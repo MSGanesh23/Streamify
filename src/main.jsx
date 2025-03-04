8761ee94d40fc2cc83cbd8efd710defa02eab2da
2300031502-Kvnbalaji <2300031502@kluniversity.in>
2025-03-04 08:25:29 +0530

    signin

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Homepage from './components/Homepage.jsx'
+import Navbar from './components/Navbar.jsx'
+import SignIn from './components/Signin.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Homepage />
+    <App />
   </StrictMode>,
 )
