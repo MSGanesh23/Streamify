50e740ea2e09684438bbebbc07910527bcd0d1e5
MSGanesh23 <saiganeshmurala@gmail.com>
2025-05-08 21:21:28 +0530

    Empty Commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
     {/* This is for Player Testing Purpose */}
     {/* <MoviePlayer fileId="1RkMo4lluZbyRegO-sgASy_Idwt7Gf2q8" onBack={() => navigate(-1)} /> */} 
     
-    <DetailsPage />
+    <App />
   </StrictMode>,
 )
 
